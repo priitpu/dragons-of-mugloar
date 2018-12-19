8a6fa3dea9dd87fea8f344cd14a155cb569998bc
Priit Puru <priit.puru@adm.ee>
2018-12-19 17:01:18 +0200

    New styling for adboard

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -11,16 +11,20 @@ class Ad extends PureComponent {
     } = this.props;
     return (
       <div className="Ad">
-        <h3>{message}</h3>
-        <p className="Ad__expiration">Expires in {expiresIn} turns</p>
-        <p className="Ad__probability">Difficulty: {probability}</p>
-        <p className="Ad__reward"><span />{reward}</p>
-        <button
-          type="button"
-          onClick={() => onSolveAd(adId)}
-        >
-          Solve
-        </button>
+        <div className="Ad__roll Ad__roll--top" />
+        <div className="Ad__content">
+          <h3>{message}</h3>
+          <p className="Ad__expiration">Expires in {expiresIn} turns</p>
+          <p className="Ad__probability">Difficulty: {probability}</p>
+          <p className="Ad__reward"><span />{reward}</p>
+          <button
+            type="button"
+            onClick={() => onSolveAd(adId)}
+          >
+            Solve
+          </button>
+        </div>
+        <div className="Ad__roll Ad__roll--bottom" />
       </div>
     );
   }
